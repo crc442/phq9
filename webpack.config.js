0aaf5982d95c2f18c76f54480ca65b948701fe10
crc442 <EequalsMCsquare91@gmail.com>
2016-12-09 22:42:54 +0530

    configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,39 @@
 const webpack = require('webpack');
+
 const debug = process.env.NOD_ENV !== 'production';
 
-module.exports = [
-	{
-		name: 'browser',
-		entry: [
-			'./app/index.js'
-		],
-		devtool: debug ? 'inline-sourcemap' : null,
-		module: {
-			loaders: [
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					loader: 'babel-loader'
-				}
-			]
-		},
-		output: {
-			filename: 'client.min.js',
-			path: __dirname + '/public/scripts/'
-		},
-		plugins: debug ? [] : [
-			new webpack.optimize.DedupePlugin(),
-			new webpack.optimize.OccurenceOrderPlugin(),
-			new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
-		]
-	}	
-]
+module.exports = {
+  name: 'browser',
+  entry: [
+    './app/index.js',
+  ],
+  devtool: debug ? 'inline-sourcemap' : null,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: debug ? ['react-hot', 'babel-loader'] : ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public/scripts/',
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
